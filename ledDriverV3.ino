b46ad2d50d4f1d44543797c1e172504ce3285299
atran39 <akt.tran@yahoo.com>
2017-07-11 18:46:27 -0400

    Initial Commit

---
+++ ledDriverV3.ino
@@ -0,0 +1,495 @@
+#include <avr/pgmspace.h>
+#include <ffft.h>
+#include <math.h>
+#include <Adafruit_GFX.h>
+
+#ifdef __AVR_ATmega32U4__
+ #define ADC_CHANNEL 7
+#else
+ #define ADC_CHANNEL 0
+#endif
+
+#define REDPIN 9
+#define GREENPIN 10
+#define BLUEPIN 11
+
+#define FADESPEED 100     // make this higher to slow down
+
+int16_t       capture[FFT_N];    // Audio capture buffer
+complex_t     bfly_buff[FFT_N];  // FFT "butterfly" buffer
+uint16_t      spectrum[FFT_N/2]; // Spectrum output buffer
+volatile byte samplePos = 0;     // Buffer position counter
+
+
+byte
+  peak[3],      // Peak level of each column; used for falling dots
+  dotCount = 0, // Frame counter for delaying dot-falling speed
+  colCount = 0; // Frame counter for storing past column data
+  
+int
+  col[3][10],   // Column levels for the prior 10 frames
+  minLvlAvg[3], // For dynamic adjustment of low & high ends of graph,
+  maxLvlAvg[3], // pseudo rolling averages for the prior few frames.
+  colDiv[3];    // Used when filtering FFT output to 8 columns
+
+static const uint8_t PROGMEM
+  // This is low-level noise that's subtracted from each FFT output column:
+  noise[64]={ 8,6,6,5,3,4,4,4,3,4,4,3,2,3,3,4,
+              2,1,2,1,3,2,3,2,1,2,3,1,2,3,4,4,
+              3,2,2,2,2,2,2,1,3,2,2,2,2,2,2,2,
+              2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,4 },
+  // These are scaling quotients for each FFT output column, sort of a
+  // graphic EQ in reverse.  Most music is pretty heavy at the bass end.
+  eq[64]={
+    255, 175,218,225,220,198,147, 99, 68, 47, 33, 22, 14,  8,  4,  2,
+      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+  // When filtering down to 8 columns, these tables contain indexes
+  // and weightings of the FFT spectrum output values to use.  Not all
+  // buckets are used -- the bottom-most and several at the top are
+  // either noisy or out of range or generally not good for a graph.
+  
+  col0data[] = {  2,  1,  // # of spectrum bins to merge, index of first
+    111,   8 },           // Weights for each bin
+//  col1data[] = {  4,  1,  // 4 bins, starting at index 1
+//     19, 186,  38,   2 }, // Weights for 4 bins.  Got it now?
+  col2data[] = {  5,  2,
+     11, 156, 118,  16,   1 },
+  //col3data[] = {  8,  3,
+  //    5,  55, 165, 164,  71,  18,   4,   1 },
+//  col4data[] = { 11,  5,
+//      3,  24,  89, 169, 178, 118,  54,  20,   6,   2,   1 },
+//  col5data[] = { 17,  7,
+//      2,   9,  29,  70, 125, 172, 185, 162, 118, 74,
+//     41,  21,  10,   5,   2,   1,   1 },
+//  col6data[] = { 25, 11,
+//      1,   4,  11,  25,  49,  83, 121, 156, 180, 185,
+//    174, 149, 118,  87,  60,  40,  25,  16,  10,   6,
+//      4,   2,   1,   1,   1 },
+  col7data[] = { 37, 16,
+      1,   2,   5,  10,  18,  30,  46,  67,  92, 118,
+    143, 164, 179, 185, 184, 174, 158, 139, 118,  97,
+     77,  60,  45,  34,  25,  18,  13,   9,   7,   5,
+      3,   2,   2,   1,   1,   1,   1 },
+  // And then this points to the start of the data for each of the columns:
+  * const colData[]  = {
+    col0data, col2data, col7data };
+
+const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+int menuStates = 5;
+
+int r, g, b;
+
+int redPin = 9;   // Red LED,   connected to digital pin 9
+int grnPin = 10;  // Green LED, connected to digital pin 10
+int bluPin = 11;  // Blue LED,  connected to digital pin 11
+
+// Color arrays
+int black[3]  = { 0, 0, 0 };
+int white[3]  = { 100, 100, 100 };
+int red[3]    = { 100, 0, 0 };
+int green[3]  = { 0, 100, 0 };
+int blue[3]   = { 0, 0, 100 };
+int yellow[3] = { 40, 95, 0 };
+int dimWhite[3] = { 30, 30, 30 };
+// etc.
+
+// Set initial color
+int redVal = black[0];
+int grnVal = black[1]; 
+int bluVal = black[2];
+
+int wait = 100;      // 10ms internal crossFade delay; increase for slower fades
+int hold = 0;       // Optional hold when a color is complete, before the next crossFade
+
+// Initialize color variables
+int prevR = redVal;
+int prevG = grnVal;
+int prevB = bluVal;
+
+int led = 9;           // the pin that the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 1;    // how many points to fade the LED by
+
+int red1 = 255;
+int blue1 = 50;
+int green1 = 0;
+
+void setup() {
+  //button
+  pinMode(buttonPin, INPUT);
+
+  //ledTest
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+
+  //colorCrossfader
+  pinMode(redPin, OUTPUT);   // sets the pins as output
+  pinMode(grnPin, OUTPUT);   
+  pinMode(bluPin, OUTPUT); 
+
+  //music
+  uint8_t i, j, nBins, binNum, *data;
+
+  memset(peak, 0, sizeof(peak));
+  memset(col , 0, sizeof(col));
+
+  for(i=0; i<3; i++) {
+    minLvlAvg[i] = 0;
+    maxLvlAvg[i] = 512;
+    data         = (uint8_t *)pgm_read_word(&colData[i]);
+    nBins        = pgm_read_byte(&data[0]) + 2;
+    binNum       = pgm_read_byte(&data[1]);
+    for(colDiv[i]=0, j=2; j<nBins; j++)
+      colDiv[i] += pgm_read_byte(&data[j]);
+  }
+
+  // Init ADC free-run mode; f = ( 16MHz/prescaler ) / 13 cycles/conversion 
+  ADMUX  = ADC_CHANNEL; // Channel sel, right-adj, use AREF pin
+  ADCSRA = _BV(ADEN)  | // ADC enable
+           _BV(ADSC)  | // ADC start
+           _BV(ADATE) | // Auto trigger
+           _BV(ADIE)  | // Interrupt enable
+           _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // 128:1 / 13 = 9615 Hz
+  ADCSRB = 0;                // Free run mode, no high MUX bit
+  DIDR0  = 1 << ADC_CHANNEL; // Turn off digital input for ADC pin
+  TIMSK0 = 0;                // Timer0 off
+
+  sei(); // Enable interrupts
+}
+
+
+void loop() {
+  
+  checkButton();
+  
+  if (buttonPushCounter == 0) {
+    //run ledDriver
+    musicDriver();
+  }
+
+  else if (buttonPushCounter == 1) { 
+    //run colorCrossfader
+    colorCrossfader();
+  }
+
+  else if (buttonPushCounter == 2) { 
+    //run ledTest
+    ledTest();
+  }
+
+  else if (buttonPushCounter == 3) { 
+    //run moodLighting
+    moodLighting();
+  }
+
+  else if (buttonPushCounter == 4){
+    
+  }
+
+  else {
+    analogWrite(REDPIN, 0); 
+    analogWrite(GREENPIN, 0); 
+    analogWrite(BLUEPIN, 0);
+  }
+}
+
+
+
+
+
+
+void musicDriver() {
+  uint8_t  i, x, L, *data, nBins, binNum, weighting, c;
+  uint16_t minLvl, maxLvl;
+  int      level, y, sum;
+
+  while(ADCSRA & _BV(ADIE)); // Wait for audio sampling to finish
+
+  fft_input(capture, bfly_buff);   // Samples -> complex #s
+  samplePos = 0;                   // Reset sample counter
+  ADCSRA |= _BV(ADIE);             // Resume sampling interrupt
+  fft_execute(bfly_buff);          // Process complex data
+  fft_output(bfly_buff, spectrum); // Complex -> spectrum
+
+  // Remove noise and apply EQ levels
+  for(x=0; x<FFT_N/2; x++) {
+    L = pgm_read_byte(&noise[x]);
+    spectrum[x] = (spectrum[x] <= L) ? 0 :
+      (((spectrum[x] - L) * (256L - pgm_read_byte(&eq[x]))) >> 8);
+  }
+
+  for(x=0; x<3; x++) {
+    data   = (uint8_t *)pgm_read_word(&colData[x]);
+    nBins  = pgm_read_byte(&data[0]) + 2;
+    binNum = pgm_read_byte(&data[1]);
+    for(sum=0, i=2; i<nBins; i++)
+      sum += spectrum[binNum++] * pgm_read_byte(&data[i]); // Weighted
+    col[x][colCount] = sum / colDiv[x];                    // Average
+    minLvl = maxLvl = col[x][0];
+    for(i=1; i<10; i++) { // Get range of prior 10 frames
+      if(col[x][i] < minLvl)      minLvl = col[x][i];
+      else if(col[x][i] > maxLvl) maxLvl = col[x][i];
+    }
+    // minLvl and maxLvl indicate the extents of the FFT output, used
+    // for vertically scaling the output graph (so it looks interesting
+    // regardless of volume level).  If they're too close together though
+    // (e.g. at very low volume levels) the graph becomes super coarse
+    // and 'jumpy'...so keep some minimum distance between them (this
+    // also lets the graph go to zero when no sound is playing):
+    if((maxLvl - minLvl) < 8) maxLvl = minLvl + 8;
+    minLvlAvg[x] = (minLvlAvg[x] * 7 + minLvl) >> 3; // Dampen min/max levels
+    maxLvlAvg[x] = (maxLvlAvg[x] * 7 + maxLvl) >> 3; // (fake rolling average)
+
+    // Second fixed-point scale based on dynamic min/max levels:
+    level = 10L * (col[x][colCount] - minLvlAvg[x]) /
+      (long)(maxLvlAvg[x] - minLvlAvg[x]);
+
+    // Clip output and convert to byte:
+    if(level < 0L)      c = 0;
+    else if(level > 10) c = 10; // Allow dot to go a couple pixels off top
+    else                c = (uint8_t)level;
+
+    if(c > peak[x]) peak[x] = c; // Keep dot on top
+
+  }
+
+  writeStrip();
+
+  // Every third frame, make the peak pixels drop by 1:
+  if(++dotCount >= 3) {
+    dotCount = 0;
+    for(x=0; x<3; x++) {
+      if(peak[x] > 0) peak[x]--;
+    }
+  }
+  
+  if(++colCount >= 10) colCount = 0;
+}
+
+
+
+
+
+void colorCrossfader() {
+  crossFade(red);
+  crossFade(green);
+  crossFade(blue);
+  crossFade(yellow);
+}
+
+
+
+
+
+
+
+void ledTest() {
+  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+}
+
+
+
+
+void moodLighting() {
+  analogWrite(BLUEPIN, blue1);
+  analogWrite(REDPIN, red1);
+  analogWrite(GREENPIN, green1);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  blue1 = brightness;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness == 0 || brightness == 100) {
+    fadeAmount = -fadeAmount ;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(100);
+}
+
+
+
+int checkButton() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      if (buttonPushCounter < menuStates) {
+        buttonPushCounter++;
+      }
+
+      else {
+        buttonPushCounter = 0;
+      }
+
+      Serial.print("Menu State:  ");
+      Serial.println(buttonPushCounter);
+      
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state,
+  //for next time through the loop
+  lastButtonState = buttonState;
+}
+
+ISR(ADC_vect) { // Audio-sampling interrupt
+  static const int16_t noiseThreshold = 4;
+  int16_t              sample         = ADC; // 0-1023
+
+  capture[samplePos] =
+    ((sample > (512-noiseThreshold)) &&
+     (sample < (512+noiseThreshold))) ? 0 :
+    sample - 512; // Sign-convert for FFT; -512 to +511
+
+  if(++samplePos >= FFT_N) ADCSRA &= ~_BV(ADIE); // Buffer full, interrupt off
+}
+
+int peakToPwn(int column) {
+  long columnVal = (long)peak[column];
+  long pwnVal = columnVal*25.5;
+  return pwnVal;
+}
+
+
+void writeStrip() {
+  analogWrite(REDPIN, peakToPwn(0)); 
+  analogWrite(GREENPIN, peakToPwn(1)); 
+  analogWrite(BLUEPIN, peakToPwn(2)); 
+  Serial.print("Columns: ");
+  Serial.print("Bass: ");
+  Serial.print(col[0][0]);
+//  Serial.print(" | ");
+//  Serial.print("Mid: ");
+//  Serial.print(col[1][0]);
+//  Serial.print(" | ");
+//  Serial.print("Highs: ");
+//  Serial.print(col[2][0]);
+  Serial.print("\n");
+  
+//  Serial.print("Red: ");
+//  Serial.print(peakToPwn(0));
+//  Serial.print(" | ");
+//  Serial.print("Green: ");
+//  Serial.print(peakToPwn(1));
+//  Serial.print(" | ");
+//  Serial.print("Blue: ");
+//  Serial.print(peakToPwn(2));
+//  Serial.print("\n");
+}
+
+int calculateStep(int prevValue, int endValue) {
+  int step = endValue - prevValue; // What's the overall gap?
+  if (step) {                      // If its non-zero, 
+    step = 1020/step;              //   divide by 1020
+  } 
+  return step;
+}
+
+/* The next function is calculateVal. When the loop value, i,
+*  reaches the step size appropriate for one of the
+*  colors, it increases or decreases the value of that color by 1. 
+*  (R, G, and B are each calculated separately.)
+*/
+
+int calculateVal(int step, int val, int i) {
+
+  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+    if (step > 0) {              //   increment the value if step is positive...
+      val += 1;           
+    } 
+    else if (step < 0) {         //   ...or decrement it if step is negative
+      val -= 1;
+    } 
+  }
+  // Defensive driving: make sure val stays in the range 0-255
+  if (val > 255) {
+    val = 255;
+  } 
+  else if (val < 0) {
+    val = 0;
+  }
+  return val;
+}
+
+/* crossFade() converts the percentage colors to a 
+*  0-255 range, then loops 1020 times, checking to see if  
+*  the value needs to be updated each time, then writing
+*  the color values to the correct pins.
+*/
+
+void crossFade(int color[3]) {
+  // Convert to 0-255
+  int R = (color[0] * 255) / 100;
+  int G = (color[1] * 255) / 100;
+  int B = (color[2] * 255) / 100;
+
+  int stepR = calculateStep(prevR, R);
+  int stepG = calculateStep(prevG, G); 
+  int stepB = calculateStep(prevB, B);
+
+  for (int i = 0; i <= 1020; i++) {
+    redVal = calculateVal(stepR, redVal, i);
+    grnVal = calculateVal(stepG, grnVal, i);
+    bluVal = calculateVal(stepB, bluVal, i);
+
+    analogWrite(redPin, redVal);   // Write current values to LED pins
+    analogWrite(grnPin, grnVal);      
+    analogWrite(bluPin, bluVal); 
+
+    delay(wait); // Pause for 'wait' milliseconds before resuming the loop
+  }
+  // Update current values for next loop
+  prevR = redVal; 
+  prevG = grnVal; 
+  prevB = bluVal;
+}
